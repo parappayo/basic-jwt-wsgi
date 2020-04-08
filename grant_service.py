755b752a8a8080dfcd96d44943efe27d84d607e9
Jason Estey <esteyj@yahoo.com>
2020-04-07 19:03:27 -0700

    grant service now checks user credentials

--- grant_service.py
+++ grant_service.py
@@ -1,5 +1,31 @@
 import falcon
-import basic_auth
+from psycopg2 import connect, sql
+import basic_auth, password_crypto
+
+
+# TODO: fetch db name, username, password from local secrets
+# it also needs to be shared by the script that builds the db docker image
+db_connection_string = "dbname='postgres' user='postgres' host='localhost' password='adminpass'"
+
+
+def get_user_credentials(db_connection, username):
+    db_cursor = db_connection.cursor()
+
+    query = sql.SQL(
+        "SELECT {fields} FROM {table} WHERE username = {username}")
+    query = query.format(
+        table = sql.Identifier('user_credentials'),
+        fields = sql.SQL(',').join([
+            sql.Identifier('password_hash'),
+            sql.Identifier('password_salt'),
+            sql.Identifier('grants')]),
+        username = sql.Literal(username));
+
+    # can debug the query by outputting it like so
+    print(query.as_string(db_cursor))
+
+    db_cursor.execute(query)
+    return db_cursor.fetchone()
 
 
 class GrantResource:
@@ -15,7 +41,22 @@ class GrantResource:
             response.status = falcon.HTTP_401
             return
 
-        # TODO: now read from Postgres and validate
+        with connect(db_connection_string) as db_connection:
+            user_credentials = get_user_credentials(db_connection, username)
+
+            if not user_credentials:
+                response.status = falcon.HTTP_403
+                return
+
+            password_hash, password_salt, grants = user_credentials
+            password_hash = bytes(password_hash)
+            password_salt = bytes(password_salt)
+
+            if not password_crypto.validate(password, password_salt, password_hash):
+                response.status = falcon.HTTP_403
+                return
+
+        # TODO: create the JWT
 
         response.body = '{"message": "got here"}'
         response.status = falcon.HTTP_200
